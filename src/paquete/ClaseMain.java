aa8fecce34407e87db76d70150fe7042f777c6a7
Santiago Leonardis <santiagoleonardis@gmail.com>
2022-11-27 18:24:58 -0300

    Primer commit

---
+++ src/paquete/ClaseMain.java
@@ -0,0 +1,21 @@
+package paquete;
+
+public class ClaseMain {
+
+	public static void main(String[] args) {
+		
+		int num = 5;
+		long num2=1000;
+		double num3 = 10.25D;
+		boolean bul= true;
+		String palabra= "Hola mundo";
+		
+		System.out.println(num);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(bul);
+		System.out.println(palabra);
+
+	}
+
+}
